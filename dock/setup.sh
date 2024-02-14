db6d9e3408b5030a5fb0beb3744a69850c74c8e2
Juri Saltbacka <juri.saltbacka@gmail.com>
2024-02-14 12:59:22 +0200

    added apps_to_add to dock setup

--- dock/setup.sh
+++ dock/setup.sh
@@ -23,9 +23,25 @@ apps_to_remove=(
     "System Settings"
 )
 
-for app in "${apps_to_remove[@]}"; do
-    echo "Removing $app"
-    dockutil --remove "$app" --no-restart --allhomes 1&> /dev/null
+apps_to_add=(
+    "/Applications/Arc.app"
+    "/Applications/Spark Desktop.app"
+    "/Applications/Rise.app"
+    "/Applications/Slack.app"
+    "/System/Applications/App Store.app"
+    "/System/Applications/System Settings.app"
+    "/Applications/kitty.app"
+    "/Applications/Visual Studio Code.app"
+)
+
+for app in "${apps_to_remove[@]}" "${apps_to_add[@]}"; do
+    dockutil --remove "$app" --no-restart --allhomes
+done
+
+killall Dock
+
+for app in "${apps_to_add[@]}"; do
+    dockutil --add "$app" --no-restart
 done
 
 defaults write com.apple.dock tilesize -int 48
