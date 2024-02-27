98c81fce4372010f9a4d72fa0c31c30a25ed454a
Juri Saltbacka <juri.saltbacka@gmail.com>
2024-02-27 13:00:48 +0200

    added echo_warn utility function

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,3 +1,4 @@
 echo_colored() { echo -e "${2}$1\033[0m"; }
 echo_info()    { echo_colored "==> $1" "\033[1;36m"; }
+echo_warn()    { echo_colored "==> $1" "\033[1;33m"; }
 fail()         { echo_colored "$1" "\033[1;31m"; exit 1; }
