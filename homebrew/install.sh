0e87060122b109a858782ca8e12a3dec2ca00bca
Juri Saltbacka <juri.saltbacka@gmail.com>
2024-08-22 20:24:33 +0300

    cleanup

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-. $DOTFILES/lib/functions.sh
+. "$DOTFILES"/lib/functions.sh
 
 if ! type "brew" >/dev/null 2>&1; then
     echo -e "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"" | pbcopy
--- lib/functions.sh
+++ lib/functions.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
 echo_colored() { echo -e "${2}$1\033[0m"; }
-echo_info()    { echo_colored "==> $1" "\033[1;36m"; }
-echo_warn()    { echo_colored "==> $1" "\033[1;33m"; }
-fail()         { echo_colored "$1" "\033[1;31m"; exit 1; }
+echo_info() { echo_colored "==> $1" "\033[1;36m"; }
+echo_warn() { echo_colored "==> $1" "\033[1;33m"; }
+fail() {
+    echo_colored "$1" "\033[1;31m"
+    exit 1
+}
